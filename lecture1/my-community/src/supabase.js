f71f8e7b8a27068e18cd083adbb375ee6e766385
hjeee1027-png <hjeee1027-png@users.noreply.github.com>
2026-04-26 00:16:58 +0900

    fix: my-community supabase 키 하드코딩으로 변경

--- lecture1/my-community/src/supabase.js
+++ lecture1/my-community/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://fkwvqqxtgmptyuznmibw.supabase.co',
+  'sb_publishable_DZuw3VHX0yqg5w9389vMow_zCNk440-'
+)
